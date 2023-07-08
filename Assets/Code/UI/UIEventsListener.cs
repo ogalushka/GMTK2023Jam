45e2df8d4b6988d3f436145614ba64e258b0e92b
Oleksandr Galushka <bugblender@gmail.com>
2023-07-08 11:46:25 +0300

    Added unit selection button

---
+++ Assets/Code/UI/UIEventsListener.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Assets.Code.UI
+{
+    [RequireComponent(typeof(UIDocument))]
+    public class UIEventsListener : MonoBehaviour
+    {
+        private const string activeButtonClass = "UnitButtonActive";
+
+        private Button activeButton;
+        private Button basicUnitButton;
+
+        private void OnEnable()
+        {
+            var uiDocument = GetComponent<UIDocument>();
+            basicUnitButton = uiDocument.rootVisualElement.Q<Button>("BaseUnit");
+            basicUnitButton.RegisterCallback<ClickEvent>(OnBasicUnitButtonClicked);
+        }
+
+        public void OnBasicUnitButtonClicked(ClickEvent e)
+        {
+            
+            if (activeButton == basicUnitButton)
+            {
+                DisableActiveButton();
+                return;
+            }
+
+            DisableActiveButton();
+
+            basicUnitButton.AddToClassList(activeButtonClass);
+            activeButton = basicUnitButton;
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(1))
+            {
+                if (activeButton != null)
+                {
+                    activeButton.RemoveFromClassList(activeButtonClass);
+                    activeButton = null;
+                }
+            }
+        }
+
+        private void DisableActiveButton()
+        {
+            if (activeButton != null)
+            {
+                activeButton.RemoveFromClassList(activeButtonClass);
+                activeButton = null;
+            }
+        }
+    }
+}
